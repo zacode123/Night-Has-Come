a5f295a55ebc679fb191144d527bce435a6ed97d
zacode123 <zuliabegum0@gmail.com>
2026-03-09 19:39:37 +0530

    Update and rename LongPress.ts to longPress.ts

--- hooks/longPress.ts
+++ hooks/longPress.ts
@@ -1,13 +1,19 @@
 import { useRef } from 'react';
 
 export function useLongPress(callback: () => void, ms = 500) {
-  const timer = useRef<NodeJS.Timeout>();
+  const timer = useRef<number | undefined>(undefined);
+
   const start = () => {
-    timer.current = setTimeout(callback, ms);
+    timer.current = window.setTimeout(callback, ms);
   };
+
   const clear = () => {
-    if (timer.current) clearTimeout(timer.current);
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = undefined;
+    }
   };
+
   return {
     onMouseDown: start,
     onMouseUp: clear,
